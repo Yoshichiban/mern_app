7ef6efd9e9d786b815d035d187632230a00f08d3
yoshichiban <jdcstarita@gmail.com>
2023-02-12 17:18:44 +0800

    initial setup of project

--- README.md
+++ README.md
@@ -3,3 +3,65 @@
 ## Notes
     "type": "module" 
 on *package.json* was added so we can use *import* statements instead of *require* statements
+
+Before writing the APIs, look at the final build or mocks or whatever you have. You want to see how you're going to organize your data models. Data models are ways of organizing your data in nice, succinct categories... making sure they are separate from each other and we can make relations between each other. We're going to be using MongoDB which is a little more freeform compared to SQL type database where everything is about relations.
+
+TLDR: Have a good *data* structure before coding.
+
+Probably the hardest part of this project:
+Authentication - Registration/Login
+Authorization - Make sure someone's logged in before you perform certain actions
+
+# Data Models
+    User
+        Location
+        Occupation
+        Number of Views
+        Impressions
+        Social Profiles
+    Posts
+        User
+        Description
+        Friends(boolean)
+        User PFP
+        Image
+        Number of likes
+        Comments
+
+*Use lucid.app*
+Data Structure + Relationships
+
+## Technologies Used
+
+MERN Stack
+MongoDB< ExpressJS, ReactJS, NodeJS
+
+Frontend
+ReactJS
+React Router - navigation
+Formik + yup - form and form validation
+Redux Toolkit - state management
+Redux Persist - store in local storage
+React dropzone - image uploads
+
+Backend
+NodeJS - runtime
+Express - backend framework
+Mongoose - managing MongoDB
+JSON Web Token(JWT) - Authentication
+Multer - File uploads
+
+Industry Standards
+
+npm i -g nodemon
+npm i express body-parser bcrypt cors dotenv gridfs-stream multer multer-gridfs-storage helmet morgan jsonwebtoken mongoose
+
+body-parser(to process request body)
+bcrypt(password encryption)
+cors(cross origin request)
+dotenv(environment variable)
+gridfs-stream multer multer-gridfs-storage(file upload)
+helmet(request safety)
+morgan(login)
+jsonwebtoken(authentication)
+mongoose(MongoDB access)
---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS (Backend Configurations and Middleware)
+
+Middleware is basically something that runs between different requests; functions
+*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); //this is only when you use "type":"modules"
+dotenv.config();
+const app = express(); //invoke express app so we can use our middleware
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); //set the directory of where we keep our assets; in our case it will be the images that we store; storing it locally, but in a real life production app we would want to store it in an actual storage file directory or cloud storage like Amazon S3; we're storing locally to simplify things for now
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+    /* 
+    all configurations are just coming from package instructions and was taken from multer's github repo;
+    this is how you can save your files;
+    anytime someone uploads a file to your website then it's gonna say destination and saved into the folder(public/assets, in this case)
+    */
+});
+const upload = multer({ storage }); //this will help us save files
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
